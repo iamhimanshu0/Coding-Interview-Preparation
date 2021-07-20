23d277cfc8cb0c0200f8768eca97f54cf73baa72
iamhimanshu0 <himanshu.tripathi16.ht@gmail.com>
2021-07-20 16:01:26 +0530

    delete unnecessary file

--- 1_Recursion/recursion.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    assert n >= 0 and int(n) == n, "The number must be positve integer only!"
-    if n == 0 or n == 1:
-        return 1
-    return n * factorial(n-1)
-
-
-print(factorial(5))
