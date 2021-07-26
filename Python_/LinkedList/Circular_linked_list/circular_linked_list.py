31a16c92c0b87f5f65b13496ad2d68e68384b436
iamhimanshu0 <himanshu.tripathi16.ht@gmail.com>
2021-07-26 12:23:19 +0530

    Circular linked list

---
+++ Python_/LinkedList/Circular_linked_list/circular_linked_list.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            self.head.next = self.head
+        else:
+            new_node = Node(data)
+            cur = self.head
+            while cur.next != self.head:
+                cur = cur.next
+
+            cur.next = new_node
+            new_node.next = self.head
+
+    def prepend(self, data):
+        new_node = Node(data)
+        cur = self.head
+        new_node.next = self.head
+
+    def print_list(self):
+        cur = self.head
+
+        while cur:
+            print(cur.data)
+            cur = cur.next
+            if cur == self.head:
+                break
+
+
+c = CircularLinkedList()
+c.append(1)
+c.append(2)
+c.append(3)
+c.append(4)
+
+c.print_list()
