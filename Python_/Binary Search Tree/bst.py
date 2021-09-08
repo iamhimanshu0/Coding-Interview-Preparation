b2ac4303fb4ae59513ff287c33f14812f5451413
himanshu <iamhimanshu0>
2021-09-08 12:57:35 +0530

    bst

--- Python_/Binary Search Tree/bst.py
+++ Python_/Binary Search Tree/bst.py
@@ -51,10 +51,22 @@ class BST:
             else:
                 # node to deleted if 2 child
                 if currentNode.left is not None and currentNode.right is not None:
-                    currentNode.value = currentNode.right.getMinValue()
                     # currentNode.value = smallest value of right subtree
+                    currentNode.value = currentNode.right.getMinValue()
+                    # delete that node 
                     currentNode.right.remove(currentNode.value, currentNode)
-
+                # if current node is none
+                elif parentNode is None:
+                    if currentNode.left is not None:
+                        currentNode.value = currentNode.left.value
+                        currentNode.right = currentNode.left.right
+                        currentNode.left  = currentNode.left.left
+                    elif currentNode.right is not None:
+                        currentNode.value = currentNode.right.value
+                        currentNode.left = currentNode.right.left
+                        currentNode.right  = currentNode.right.right
+                    else:
+                        currentNode.value = None
                 # if we don't have 2 child node
                 elif parentNode.left == currentNode:
                     parentNode.left = (
@@ -62,3 +74,17 @@ class BST:
                         if currentNode.left is not None
                         else currentNode.right
                     )
+                elif parentNode.right == currentNode:
+                    parentNode.right = (
+                        currentNode.left 
+                        if currentNode.len is not None 
+                        else currentNode.right
+                    )
+                break
+        return self
+
+    def getMinValue(self):
+        currentNode = self 
+        while currentNode.left is not None:
+            currentNode = currentNode.left
+        return currentNode.value
