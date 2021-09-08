3b3d255740ea002734fcfe2686fd0f0f934dffc0
Himanshu <himanshutripathi16.ht@gmail.com>
2021-09-08 10:50:27 +0530

    adding new files

---
+++ Python_/Binary Search Tree/bst.py
@@ -0,0 +1,64 @@
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Average:: O(log(n)) time | O(1) space
+    # worst :: O(n) time | O(1) space
+    def insert(self, value):
+        currentNode = self
+        while True:
+            if value < currentNode.value:
+                # get left subtree
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+
+            else:
+                # get right subtree
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+
+        return self
+
+    # Average:: O(log(n)) time | O(1) space
+    # worst :: O(n) time | O(1) space
+    def contains(self, value):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                currentNode = currentNode.right
+            else:
+                return True
+
+    def remove(self, value, parentNode=None):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.right
+            else:
+                # node to deleted if 2 child
+                if currentNode.left is not None and currentNode.right is not None:
+                    currentNode.value = currentNode.right.getMinValue()
+                    # currentNode.value = smallest value of right subtree
+                    currentNode.right.remove(currentNode.value, currentNode)
+
+                # if we don't have 2 child node
+                elif parentNode.left == currentNode:
+                    parentNode.left = (
+                        currentNode.left
+                        if currentNode.left is not None
+                        else currentNode.right
+                    )
