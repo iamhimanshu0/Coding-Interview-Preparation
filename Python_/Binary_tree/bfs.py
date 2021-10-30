4da77245438a88d3de049833a466df700e8d8848
himanshu <iamhimanshu0>
2021-10-30 15:21:36 +0530

    simple bfs

--- Python_/Binary_tree/bfs.py
+++ Python_/Binary_tree/bfs.py
@@ -1,6 +1,13 @@
 from collections import deque
 
 
+class Node:
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+
+
 def breadth_first_values(root):
     if not root:
         return []
@@ -20,3 +27,23 @@ def breadth_first_values(root):
             queue.append(node.right)
 
     return values
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+
+#     1
+#    / \
+#   2   3
+#  / \ / \
+# 4  5 6  7
+
+print(breadth_first_values(root))
