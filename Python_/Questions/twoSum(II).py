149131b6f9ef26b767730880e56b2a308ed62096
himanshu <iamhimanshu0>
2022-01-05 19:28:18 +0530

    adding twoSum question

---
+++ Python_/Questions/twoSum(II).py
@@ -0,0 +1,60 @@
+"""
+    Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+    Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+    The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+    Input: numbers = [2,7,11,15], target = 9
+    Output: [1,2]
+    Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+    Input: numbers = [2,3,4], target = 6
+    Output: [1,3]
+    Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+    Input: numbers = [-1,0], target = -1
+    Output: [1,2]
+    Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+
+    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+"""
+# number = [2,7,11,15]
+# target = 9
+
+number = [-1,0]
+target = -1
+
+# two-pointer
+def twoSum1(numbers, target):
+    l, r = 0, len(numbers)-1
+    while l < r:
+        s = numbers[l] + numbers[r]
+        if s == target:
+            return [l+1, r+1]
+        elif s < target:
+            l += 1
+        else:
+            r -= 1
+ 
+# dictionary           
+def twoSum2(numbers, target):
+    dic = {}
+    for i, num in enumerate(numbers):
+        if target-num in dic:
+            return [dic[target-num]+1, i+1]
+        dic[num] = i
+ 
+# binary search        
+def twoSum(numbers, target):
+    for i in range(len(numbers)):
+        l, r = i+1, len(numbers)-1
+        tmp = target - numbers[i]
+        while l <= r:
+            mid = l + (r-l)//2
+            if numbers[mid] == tmp:
+                return [i+1, mid+1]
+            elif numbers[mid] < tmp:
+                l = mid+1
+            else:
+                r = mid-1
