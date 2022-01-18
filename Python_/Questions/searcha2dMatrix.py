ca90f1a0cf87a59a7910d4c42c1555b81367bcd2
himanshu <iamhimanshu0>
2022-01-18 17:47:21 +0530

    add other styles for search in a 2d matrix

--- Python_/Questions/searcha2dMatrix.py
+++ Python_/Questions/searcha2dMatrix.py
@@ -16,7 +16,7 @@ Output: false
 matrix = [[1,3,5,7],
           [10,11,16,20],
           [23,30,34,60]]
-target = 3
+target = 89
 
 # def searchMatrix(matrix, target):
 #     ROW, COL = len(matrix), len(matrix[0])
@@ -27,33 +27,50 @@ target = 3
 #                 return True
 #     return False
 
+# def searchMatrix(matrix, target):
+#     ROWS, COLS = len(matrix), len(matrix[0])
+
+#     top, bot = 0, ROWS -1 
+#     while top  <= bot:
+#         row = (top+bot)//2
+#         if target > matrix[row][-1]:
+#             top = row +1 
+#         elif target < matrix[row][0]:
+#             bot = row - 1 
+#         else:
+#             break
+
+#     if not (top <= bot):
+#         return False
+    
+#     row = (top + bot) //2
+#     l, r = 0, COLS -1 
+#     while l <= r:
+#         m = (l+r) //2 
+#         if target > matrix[row][m]:
+#             l = m +1
+#         elif target > matrix[row][m]:
+#             r = m -1
+#         else:
+#             return True
+#     return False
+
+
 def searchMatrix(matrix, target):
     ROWS, COLS = len(matrix), len(matrix[0])
-
-    top, bot = 0, ROWS -1 
-    while top  <= bot:
-        row = (top+bot)//2
-        if target > matrix[row][-1]:
-            top = row +1 
-        elif target < matrix[row][0]:
-            bot = row - 1 
-        else:
-            break
-
-    if not (top <= bot):
-        return False
     
-    row = (top + bot) //2
-    l, r = 0, COLS -1 
+    l, r = 0, ROWS*COLS - 1
+
     while l <= r:
-        m = (l+r) //2 
-        if target > matrix[row][m]:
-            l = m +1
-        elif target > matrix[row][m]:
-            r = m -1
-        else:
+        mid = (l+r)//2
+        if matrix[mid//COLS][mid%COLS] == target:
             return True
+        elif matrix[mid//COLS][mid%COLS] < target:
+            l = mid +1
+        elif matrix[mid//COLS][mid%COLS] > target:
+            r = mid - 1
     return False
+
 
 
 print(
