5a9c006a1e9abdef41a1ee92d3fd125bd704345b
himanshu <iamhimanshu0>
2022-02-07 12:25:22 +0530

    valid parentheses

---
+++ Python_/DataStructure/validParentheses.py
@@ -0,0 +1,43 @@
+"""
+# https://leetcode.com/problems/valid-parentheses/
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+Input: s = "()"
+Output: true
+
+Input: s = "()[]{}"
+Output: true
+
+Input: s = "(]"
+Output: false
+"""
+
+s = "(){}[]"
+
+def isValid(s):
+    stack = []
+    dict = {
+        "]":"[",
+        "}":"{",
+        ")":"("
+    }
+
+    for char in s:
+        if char in dict.values():
+            stack.append(char)
+        elif char in dict.keys():
+            if stack == [] or dict[char] != stack.pop():
+                return False
+        else:
+            return False
+    return stack == []
+
+
+print(
+    isValid(s)
+)
