bdd23795ba82c820fd618b8adbda6f7df79673d4
himanshu <iamhimanshu0>
2022-02-11 16:32:52 +0530

    insert into a binary tree

---
+++ Python_/DataStructure/insertIntoABinaryTree.py
@@ -0,0 +1,53 @@
+"""
+You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+Input: root = [4,2,7,1,3], val = 5
+Output: [4,2,7,1,3,5]
+
+Input: root = [40,20,60,10,30,50,70], val = 25
+Output: [40,20,60,10,30,50,70,null,null,25]
+
+Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+Output: [4,2,7,1,3,5]
+"""
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def insertIntoBST(root, val):
+    if not root: return TreeNode(val)
+    def insert(node, val):
+        if val > node.val:
+            if not node.right:
+                node.right = TreeNode(val)
+            else:
+                insert(node.right, val)
+        else:
+            if not node.left:
+                node.left = TreeNode(val)
+            else:
+                insert(node.left.val)
+
+    insert(root, val)
+    return root
+    
+        
+
+
+
+ 
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.right = TreeNode(7)
+
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(3)
+
+
+
