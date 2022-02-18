52c0c81727acfe0bbe1a10cd7c9f83f19b9c5274
himanshu <iamhimanshu0>
2022-02-18 10:59:13 +0530

    add string

---
+++ Python_/DataStructure/addStrings.py
@@ -0,0 +1,23 @@
+"""
+# https://leetcode.com/problems/add-strings/
+
+Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+Input: num1 = "11", num2 = "123"
+Output: "134"
+
+Input: num1 = "456", num2 = "77"
+Output: "533"
+
+Input: num1 = "0", num2 = "0"
+Output: "0"
+"""
+num1 = "11"; num2 = "123"
+
+def addStrings(num1, num2):
+    return str(int(num1)+int(num2))
+
+
+print(addStrings(num1, num2))
