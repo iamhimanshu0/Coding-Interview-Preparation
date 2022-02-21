87150e346b794877da30ebdf6f8fe3662369b89a
himanshu <iamhimanshu0>
2022-02-21 10:01:39 +0530

    group Anagrams 2

--- Python_/DataStructure/groupAnagrams.py
+++ Python_/DataStructure/groupAnagrams.py
@@ -20,18 +20,29 @@ from collections import defaultdict
 
 strs = ["eat","tea","tan","ate","nat","bat"]
 
-def groupAnagrams(strs):
-    res = defaultdict(list)  # mapping charCount to list of Anagrams
+# def groupAnagrams(strs):
+#     res = defaultdict(list)  # mapping charCount to list of Anagrams
 
-    for s in strs:
-        count = [0]*26
+#     for s in strs:
+#         count = [0]*26
 
-        for c in s:
-            count[ord(c) - ord("a")] +=1
+#         for c in s:
+#             count[ord(c) - ord("a")] +=1
         
-        res[tuple(count)].append(s)
+#         res[tuple(count)].append(s)
     
-    return res.values()
+#     return res.values()
 
+
+def groupAnagrams(strs):
+    anagram = {}
+    for word in strs:
+        currentWord = "".join(sorted(word))
+        if currentWord in anagram:
+            anagram[currentWord].append(word)
+        else:
+            anagram[currentWord] = [word]
+
+    return list(anagram.values())
 
 print(groupAnagrams(strs))
