dd4ec5b1e11df72e1e63cdd723ecef9372233797
himanshu <iamhimanshu0>
2022-03-04 13:28:50 +0530

    kthLargetElement.py

--- Python_/DataStructure/kthLargetElement.py
+++ Python_/DataStructure/kthLargetElement.py
@@ -14,6 +14,7 @@ Output: 4
 
 # O(nlgn) time
 import heapq
+import random
 
 
 def findKthLargest1(nums, k):
@@ -83,3 +84,42 @@ def partition(self, nums, l, r):
         l += 1
     nums[low], nums[r] = nums[r], nums[low]
     return low
+
+"""
+If you see this problem in competition, the best way just to sort data and choose k-th largest element in O(n log n) overall complexity. However you can imagine, that it is not the good idea for real interview, where your expected do do something else. There is two classical ways how to find order statistic in linear time (yes, it has its mathematical name):
+
+Median of median approach with worst time complexity O(n), however it is quite painful to code and I do not think anyone expect that you do it.
+So-called Quick Select, which use similar idea to Quick Sort, with average complexity O(n).
+We are going to discuss Quick Select, because it is easier to code:
+
+First, we need to choose so-called pivot and partition element of nums into 3 parts: elements, smaller than pivot, equal to pivot and bigger than pivot. (sometimes two groups enough: less and more or equal)
+Next step is to see how many elements we have in each group: if k <= L, where L is size of left, than we can be sure that we need to look into the left part. If k > L + M, where M is size of mid group, than we can be sure, that we need to look into the right part. Finally, if none of these two condition holds, we need to look in the mid part, but because all elements there are equal, we can immedietly return mid[0].
+Complexity: time complexity is O(n) in average, because on each time we reduce searching range approximately 2 times. This is not strict proof, for more details you can do some googling. Space complexity is O(n) as well.
+
+
+"""
+nums = [3,2,3,1,2,4,5,5,6]; k = 4
+print("start", nums)
+def quickSortImplement(nums,k):
+    if not nums: return
+    
+    pivot = random.choice(nums)
+    print("Pivot", pivot)
+    left = [x for x in nums if x > pivot]
+    mid = [x for x in nums if x == pivot]
+    right = [x for x in nums if x < pivot]
+
+    print(left, mid, right)
+    L, M = len(left), len(mid)
+    print("Length" , L,M)
+
+    print(f"k -> {k} L -> {L} M -> {M}")
+    if k <= L:
+        return quickSortImplement(left,k)
+    elif k >L+M:
+        return quickSortImplement(right, k-L-M)
+    else:
+        return mid[0]
+
+
+print(quickSortImplement(nums, k))
