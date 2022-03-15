823df11886ff3e2d552d676f49d6361ba2b47ab4
himanshu <iamhimanshu0>
2022-03-15 18:56:16 +0530

    blind75/

---
+++ Python_/blind75/1_twosum.py
@@ -0,0 +1,31 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+"""
+nums = [2,7,11,15] 
+
+def solution(nums, target):
+    d = {}
+    for i, n in enumerate(nums):
+        print(d)
+        m = target - n
+        if m in d:
+            return [d[m],i]
+        else:
+            d[n] = i
+    
+
+print(solution(nums, 26))
