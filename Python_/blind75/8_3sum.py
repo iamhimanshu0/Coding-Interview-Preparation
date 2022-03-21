7dbf2620371c77a1ec7c03a086056d23266d72e7
himanshu <iamhimanshu0>
2022-03-21 10:12:57 +0530

    8_3sum.py

---
+++ Python_/blind75/8_3sum.py
@@ -0,0 +1,46 @@
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+
+Input: nums = []
+Output: []
+
+Input: nums = [0]
+Output: []
+"""
+nums = [-2,0,0,2,2]
+
+def threeSum(nums):
+    nums.sort()
+    result = []
+
+    for i in range(len(nums)-2):
+        if i >0 and nums[i]==nums[i-1]:
+            continue
+        l,r = i+1, len(nums)-1
+
+        while l < r:
+            s = nums[i] + nums[l]+ nums[r]
+            if s < 0:
+                l+=1
+            elif s > 0 :
+                r-=1
+            else:
+                result.append([nums[i], nums[l], nums[r]])
+                while l <r and nums[l] == nums[l+1]:
+                    l+=1
+                while l < r and nums[r] == nums[r-1]:
+                    r-=1 
+                l+=1
+                r-=1
+
+    return result
+
+        
+
+
+print(threeSum(nums))
