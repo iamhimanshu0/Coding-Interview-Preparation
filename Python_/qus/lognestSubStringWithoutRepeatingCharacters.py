610397822f91b751df742fe204810966dc386a54
himanshu <iamhimanshu0>
2022-04-12 20:59:38 +0530

    lognestSubStringWithoutRepeatingCharacters.py

--- Python_/qus/lognestSubStringWithoutRepeatingCharacters.py
+++ Python_/qus/lognestSubStringWithoutRepeatingCharacters.py
@@ -22,13 +22,14 @@ def lengthOfLongestSubstring(s):
     charSet = set()
     l = 0
     result = 0
-    for r in range(len(s)):
-        while s[r] in charSet:
+    for i in range(len(s)):
+        while s[i] in charSet:
             charSet.remove(s[l])
             l+=1
-        charSet.add(s[r])
-        result = max(result, r-l+1)
+        charSet.add(s[i])
+        result = max(result, i-l+1)
     return result
+
 
 print(
     lengthOfLongestSubstring(s)
