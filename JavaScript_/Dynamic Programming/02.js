71261b3408a950b7c644df7cf6176aacbea7ecce
himanshu <iamhimanshu0>
2022-05-15 21:58:42 +0530

    grid traveler

--- JavaScript_/Dynamic Programming/02.js
+++ JavaScript_/Dynamic Programming/02.js
@@ -34,3 +34,19 @@ const gridTravelerMemo = (m, n, memo = {}) => {
 };
 
 console.log(gridTravelerMemo(21, 21));
+
+
+/*
+    Memoization Recipe
+
+    1. Make it work
+    - visualize the problem as a tree
+    - implement the tree using recursion
+    - test it
+
+    2. Make it efficient
+    - add a memo object
+    - add a base case to return memo values
+    - store return values into the memo
+
+*/
