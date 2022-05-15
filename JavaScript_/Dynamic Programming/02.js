76a0b46d0ca47cdb5a2361a4db431b2257780542
himanshu <iamhimanshu0>
2022-05-15 21:21:52 +0530

    Dynamic programming JavaScript

---
+++ JavaScript_/Dynamic Programming/02.js
@@ -0,0 +1,8 @@
+/*
+    Say that you are a traveler on a 2D grid. You begin the the
+    top-left corner and your goal is to travel to the bottom-right
+    corner. You may only move down or right.
+
+    In how many ways can you travel to the gole on a grid
+    with dimensions m*n? 
+*/
