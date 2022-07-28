d6d327d2ef7b8bfc76cb72806fcc1f6602b0a6a7
iamhimanshu0 <himanshu.tripathi16.ht@gmail.com>
2022-07-28 20:49:03 +0530

    maxiumSumOfNonAdjacentElements.py

--- Python_/dp/maxiumSumOfNonAdjacentElements.py
+++ Python_/dp/maxiumSumOfNonAdjacentElements.py
@@ -22,8 +22,6 @@ def maximunNonAdjacentSums(nums):
         memo[idx] = max(pick, notPick)
         return memo[idx]
 
-    dp = [-1] * len(nums)
-
     def solve_dp(idx):
 
         if idx == 0:
@@ -41,11 +39,73 @@ def maximunNonAdjacentSums(nums):
 
         return dp[idx]
 
-    return solve_dp(len(nums)-1)
+    # tabulation method (DP)
+    def tabulation():
+        dp = [-1] * len(nums)
+        dp[0] = nums[0]
+
+        for i in range(1, len(nums)):
+            take = nums[i]
+            if i > 1:
+                take += dp[i-2]
+
+            nonTake = 0 + dp[i-1]
+
+            dp[i] = max(take, nonTake)
+
+        return dp[-1]
+
+    # space optimization
+    def space_optimization():
+        prev = nums[0]
+        prev2 = 0
+
+        for i in range(1, len(nums)):
+            take = nums[i]
+            if i > 1:
+                take += prev2
+
+            nonTake = 0 + prev
+
+            curi = max(take, nonTake)
+            prev2 = prev
+            prev = curi
+
+        return prev
+
+    # return space_optimization()
+    # return solve_dp(len(nums)-1)
     return backtrack(len(nums)-1)
+
+
+def houseRobber2(nums):
+
+    def dp(n):
+        prev2 = 0
+        prev = nums[0]
+        for i in range(1, len(n)):
+            if len(n) == 1:
+                return n[0]
+            pick = n[i]
+            if i > 1:
+                pick += prev2
+
+            notPick = 0 + prev
+
+            curi = max(pick, notPick)
+            prev2 = prev
+            prev = curi
+
+        return prev
+
+    return max(dp(nums[:-1]), dp(nums[1:]))
 
 
 print(
     # maximunNonAdjacentSums([1, 2, 4]),
-    maximunNonAdjacentSums([2, 1, 4, 9])
+    # maximunNonAdjacentSums([2, 1, 4, 9])
+
+    houseRobber2([2, 3, 2]),
+    houseRobber2([1, 2, 3, 1]),
+    houseRobber2([1, 2])
 )
