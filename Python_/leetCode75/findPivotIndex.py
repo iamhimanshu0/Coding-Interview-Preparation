cb025f2bfedf4797561f28f99c724db1ab8b9396
iamhimanshu0 <himanshu.tripathi16.ht@gmail.com>
2022-08-20 22:21:58 +0530

    findPivotIndex.py

---
+++ Python_/leetCode75/findPivotIndex.py
@@ -0,0 +1,50 @@
+"""
+Given an array of integers nums, calculate the pivot index of this array.
+
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+Return the leftmost pivot index. If no such index exists, return -1.
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+
+Input: nums = [2,1,-1]
+Output: 0
+Explanation:
+The pivot index is 0.
+Left sum = 0 (no elements to the left of index 0)
+Right sum = nums[1] + nums[2] = 1 + -1 = 
+
+  |
+1,2,3
+  |
+leftsum = 1 
+rightsum = 3 
+"""
+
+
+def pivotIndex(nums):
+    left, right = 0, sum(nums)
+    for index, num in enumerate(nums):
+        right -= num
+        if left == right:
+            return index
+        left += num
+
+    return -1
+
+
+print(
+    pivotIndex([1, 7, 3, 6, 5, 6])
+)
