35e6717d8fc555401453a8ffc10e38f7fdd2c507
iamhimanshu0 <himanshu.tripathi16.ht@gmail.com>
2022-08-21 21:30:21 +0530

    subSequence.py

---
+++ Python_/leetCode75/subSequence.py
@@ -0,0 +1,27 @@
+"""
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+"""
+
+
+def isSubsequence(s, t):
+    i, j = 0, 0
+
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+
+    return i == len(s)
+
+
+print(
+    isSubsequence("abc", "ahbgdc")
+)
